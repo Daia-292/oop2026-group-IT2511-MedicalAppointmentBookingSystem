3ad69d43140f41f7b1523780c69e92b19295c583
Daia-292 <251017@astanait.edu.kz>
2026-01-22 00:47:48 +0500

    (Issen Daiana) added exceptions

---
+++ src/Exceptions/DoctorUnavailableException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class DoctorUnavailableException extends RuntimeException {
+    public DoctorUnavailableException(String message) {
+        super(message);
+    }
+}
--- src/entity/Patient.java
+++ src/entity/Patient.java
@@ -1,12 +1,12 @@
 package entity;
 
 public class Patient {
-    private final long id;
-    private final String name;
-    private final String email;
-    private final String phone;
+    private long id;
+    private String name;
+    private String email;
+    private int phone;
 
-    public Patient(long id, String name, String email, String phone) {
+    public Patient(long id, String name, String email, int phone) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -16,7 +16,10 @@ public class Patient {
     public long getId() { return id; }
     public String getName() { return name; }
     public String getEmail() { return email; }
-    public String getPhone() { return phone; }
+    public int getPhone() { return phone; }
+    public void setId(int id) { this.id = id; }
+    public void setFullName(String fullName) { this.name = name; }
+    public void setPhone(int phone) { this.phone = phone; }
 
     @Override
     public String toString() {
