7e3a6d10cf6d8450f7e93fd4d13ca70964bbada1
Daia-292 <251017@astanait.edu.kz>
2026-01-24 17:49:26 +0500

    FINAL!!

--- src/entity/Appointment.java
+++ src/entity/Appointment.java
@@ -6,21 +6,42 @@ public class Appointment {
     private final long patientId;
     private final long doctorId;
     private final LocalDateTime startTime;
-    private final AppointmentStatus status;
+    private LocalDateTime endTime;
+    private LocalDateTime createdAt;
+    private AppointmentStatus status;
 
-    public Appointment(long id, long patientId, long doctorId, LocalDateTime startTime, AppointmentStatus status) {
+    public Appointment(long id, long patientId, long doctorId, LocalDateTime startTime,LocalDateTime endTime,
+                       AppointmentStatus status, LocalDateTime createdAt) {
         this.id = id;
         this.patientId = patientId;
         this.doctorId = doctorId;
         this.startTime = startTime;
+        this.endTime = endTime;
         this.status = status;
+        this.createdAt = createdAt;
     }
+
+
+
 
     public long getId() { return id; }
     public long getPatientId() { return patientId; }
     public long getDoctorId() { return doctorId; }
     public LocalDateTime getStartTime() { return startTime; }
     public AppointmentStatus getStatus() { return status; }
+    public LocalDateTime getEndTime() { return endTime; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+
+
+
+    public long setId() { return id; }
+    public long setPatientId() { return patientId; }
+    public long setDoctorId() { return doctorId; }
+    public LocalDateTime setStartTime() { return startTime; }
+    public void setEndTime(LocalDateTime endTime) { this.endTime = endTime; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    public void setStatus(AppointmentStatus status) { this.status = status; }
+
 
     @Override
     public String toString() {
